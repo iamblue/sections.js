c45c8b3c85ded38f12381d9d34c57a9b3c98ea60
Po-Ying Chen <poying.me@gmail.com>
2013-10-15 00:00:16 +0800

    update demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,24 +1,27 @@
 var page = sections.create();
 
+page.on('changed', function (current, prev) {
+  console.log('changed');
+});
+
+page.on('progress', function (progress) {
+  console.log('progress: %s', progress);
+});
+
+page.section(1, function (section) {
+  section.on('progress', function (progress) {
+    console.log('section 1: %s', progress);
+  });
+
+  section.on('scrollIn', function (way) {
+    console.log('section 1: scrollIn. %s', way === -1 ? 'top -> bottom' : 'bottom -> top');
+  });
+
+  section.on('scrollOut', function (way) {
+    console.log('section 1: scrollOut. %s', way === 1 ? 'top -> bottom' : 'bottom -> top');
+  });
+});
+
 window.onload = function () {
   page.start();
-
-  page.on('changed', function (current, prev) {
-    console.log('changed');
-  });
-
-  page.on('progress', function (progress) {
-    console.log('progress: %s', progress);
-  });
-
-  page.get(1)
-    .on('progress', function (progress) {
-      console.log('section 1: %s', progress);
-    })
-    .on('scrollIn', function (way) {
-      console.log('section 1: scrollIn. %s', way === -1 ? 'top -> bottom' : 'bottom -> top');
-    })
-    .on('scrollOut', function (way) {
-      console.log('section 1: scrollOut. %s', way === 1 ? 'top -> bottom' : 'bottom -> top');
-    });
 };

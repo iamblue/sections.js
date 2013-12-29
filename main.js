bbc9b6c8c2adb5748abdd39183ead69f3419ff71
Po-Ying Chen <poying.me@gmail.com>
2013-12-29 17:58:42 +0800

    first section

--- main.js
+++ main.js
@@ -3,6 +3,15 @@
 
   var page = $$.create();
 
-  page.section(0, function () {
+  page.section(0, function (section) {
+    var mouse = document.querySelector('.section--first__mouse');
+
+    section.transitions([
+      {key: 'transform', start: 100, end: 0, from: 0, to: 200, format: 'translate3d(0, %spx, 0)', prefix: true, target: mouse}
+    ]);
   });
+
+  window.onload = function () {
+    page.init();
+  };
 })(this, sections);
